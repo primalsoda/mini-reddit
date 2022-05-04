19bb9d04e6da1d286b7835bc28a2a982f35053fe
primalsoda <89487117+primalsoda@users.noreply.github.com>
2022-05-04 15:10:06 -0700

    Adding new logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { icons } from './icons';
 
 const logo = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-download.com%2Fwp-content%2Fuploads%2F2016%2F06%2FReddit_logo_full_1.png&f=1&nofb=1';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={icons.best_icon} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/icons.js
@@ -0,0 +1,9 @@
+export const icons = {
+    best_icon: 'https://www.iconsdb.com/icons/preview/gray/rocket-xl.png',
+    hot_icon: 'https://www.iconsdb.com/icons/preview/gray/fire-2-xl.png',
+    new_icon: 'https://www.iconsdb.com/icons/preview/gray/new-badge-2-xl.png',
+    top_icon: 'https://www.iconsdb.com/icons/preview/gray/arrow-144-xl.png',
+    reddit_icon: 'https://www.iconsdb.com/icons/preview/gray/reddit-xl.png',
+    comment_icon: 'https://www.iconsdb.com/icons/preview/gray/comment-square-xl.png',
+    link_icon: 'https://www.iconsdb.com/icons/preview/gray/link-xl.png',
+};
